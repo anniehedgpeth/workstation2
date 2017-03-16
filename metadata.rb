9206e36407a0702d597ac008fce4bd2e70bf03ef
anniehedgpeth <annie.hedgpeth@gmail.com>
2017-03-16 20:29:37 +0000

    added inspec

--- metadata.rb
+++ metadata.rb
@@ -6,7 +6,6 @@ description 'Installs/Configures workstation2'
 long_description 'Installs/Configures workstation2'
 version '0.1.0'
 
-
 # The `issues_url` points to the location where issues for this cookbook are
 # tracked.  A `View Issues` link will be displayed on this cookbook's page when
 # uploaded to a Supermarket.
--- recipes/default.rb
+++ recipes/default.rb
@@ -3,4 +3,4 @@
 # Recipe:: default
 #
 # Copyright:: 2017, The Authors, All Rights Reserved.
-include_recipe 'workstation::disable-uac'
+include_recipe 'workstation2::disable-uac'
